e243f2b513f534c1c0e9404846d1acdada9effc3
Adam Cichoski <cjadam003@gmail.com>
2025-09-26 18:40:09 -0400

    Added javascript to get the price and add it / remove it from the total cost of the favorites. Background colors are also changed when added or removed from favorites.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 const buttons = document.querySelectorAll('.foodItem');
 const favoritesContainer = document.getElementById('favorites');
+const totalCost = document.getElementById("totalCost");
+
 let favorites = [];
+let favoritesCost = 0.00;
+
 
 buttons.forEach(button => {
     button.addEventListener('click', toggleFavorite);
@@ -8,21 +12,36 @@ buttons.forEach(button => {
 
 function toggleFavorite(event) {
     const foodItem = event.currentTarget;
+    let itemPrice = priceCentsFrom(foodItem);
     const itemName = foodItem.querySelector("h3").textContent.trim();
-
     const existingIndex = favorites.findIndex(
         fav => fav.querySelector("h3").textContent.trim() === itemName
     );
 
     if (existingIndex > -1) {
+        favoritesCost -= itemPrice;
         const removedItem = favorites.splice(existingIndex, 1)[0];
-
+        foodItem.style.backgroundColor = "antiquewhite";
         favoritesContainer.removeChild(removedItem);
-        console.log(`Removed from favorites: ${itemName}`);
+        console.log(`Removed from favorites: ${itemName} for - \$${centsToDollar(itemPrice)}`);
     } else {
+        favoritesCost += itemPrice;
+        foodItem.style.backgroundColor = "rgb(244, 212, 170)";
         const clone = foodItem.cloneNode(true);
         favorites.push(clone);
         favoritesContainer.appendChild(clone);
-        console.log(`Added to favorites: ${itemName}`);
+        console.log(`Added to favorites: ${itemName} for + \$${centsToDollar(itemPrice)}`);
     }
+    totalCost.textContent = centsToDollar(favoritesCost);
+}
+
+function centsToDollar(price){
+    return (price / 100).toFixed(2);
+}
+
+
+function priceCentsFrom(foodItem) {
+  const priceText = foodItem.querySelector("#price").textContent;
+  const dollars = parseFloat(priceText);
+  return Math.round((isNaN(dollars) ? 0 : dollars) * 100);
 }
